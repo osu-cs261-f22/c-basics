b92cca8cb787f4652eeda6186d33fd5aa790f2f7
Rob Hess <hessro@oregonstate.edu>
2022-10-05 11:18:32 -0700

    Add code from 10/5 lecture.

--- pointers.c
+++ pointers.c
@@ -1,5 +1,11 @@
 #include <stdio.h>
 
+struct student {
+    char* name;
+    int id;
+    double gpa;
+};
+
 int main() {
     int n = 16;
     int* n_ptr = &n;
@@ -17,5 +23,27 @@ int main() {
 
     int* n_ptr2 = NULL;
     printf("\n== n_ptr2: %p\n", n_ptr2);
-    *n_ptr2 = 32;
+    // *n_ptr2 = 32;
+
+    struct student s;
+    s.name = "Luke Skywalker";
+    s.id = 933111111;
+    s.gpa = 3.75;
+
+    struct student* s_ptr = &s;
+
+    // (*s_ptr).name
+    printf("\n== %s (%d) gpa: %f\n", (*s_ptr).name, (*s_ptr).id, (*s_ptr).gpa);
+    printf("== %s (%d) gpa: %f\n", s_ptr->name, s_ptr->id, s_ptr->gpa);
+
+    void* v_ptr = n_ptr;
+    printf("\n== v_ptr: %p\n", v_ptr);
+    // printf("== *v_ptr: %d\n", *v_ptr);
+    int* nv_ptr = v_ptr;
+    printf("== *nv_ptr: %d\n", *nv_ptr);
+    printf("== *(int*)v_ptr: %d\n", *(int*)v_ptr);
+
+    v_ptr = s_ptr;
+    printf("\n== v_ptr: %p\n", v_ptr);
+    printf("== s_ptr: %p\n", s_ptr);
 }
